7ac1be4ed571367efd6814a690dcb06da514f77a
brendan sudol <brendansudol@gmail.com>
2020-01-03 14:46:30 -0500

    update mapbox api

--- src/util/index.js
+++ src/util/index.js
@@ -1,39 +1,41 @@
-export const MAPBOX_KEY = 'pk.eyJ1IjoiYnJlbmRhbnN1ZG9sIiwiYSI6ImNpenJzMWdjYzAyYmQzNnFndTk0a3Q3OGwifQ.INlrzzBop-AL0pAduxuQbA'
+export const MAPBOX_KEY =
+  "pk.eyJ1IjoiYnJlbmRhbnN1ZG9sIiwiYSI6ImNrM25oNThxZzE0d2QzZ3FuaHIwcWxhYnkifQ.4ieoOfQ1fHd3pkDn1IZRKw"
 
 export const geoConfig = {
   map: {
     minZoom: 10,
     maxZoom: 16,
-    zoomControl: false,
+    zoomControl: false
   },
   cluster: {
     showCoverageOnHover: false,
     zoomToBoundsOnClick: false,
     spiderfyOnMaxZoom: true,
     maxClusterRadius: 40,
-    spiderfyDistanceMultiplier: 0.6,
-  },
+    spiderfyDistanceMultiplier: 0.6
+  }
 }
 
-const tooltip = ({ name, last_tweet }) => (`
+const tooltip = ({ name, last_tweet }) =>
+  `
   <div class='p1' style='width:300px'>
     <div class='h5 bold'>${name}</div>
     <div class='h6'>"${last_tweet.text}"</div>
   </div>
-`.trim())
+`.trim()
 
 const entry = (datum, loc) => ({
-  type: 'Feature',
+  type: "Feature",
   geometry: {
-    type: 'Point',
+    type: "Point",
     coordinates: [loc.lng, loc.lat]
   },
   properties: {
-    'marker-color': '#fc4353',
-    'marker-size': 'small',
+    "marker-color": "#fc4353",
+    "marker-size": "small",
     title: tooltip(datum),
-    info: { ...datum },
-  },
+    info: { ...datum }
+  }
 })
 
 export const formatData = data => {
